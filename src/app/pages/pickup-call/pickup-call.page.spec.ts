fa6a1294dc67614760dc856e82c618cd0f675f6f
zulkarnain faizan <zulkarnainfaizan@gmail.com>
2022-02-06 10:34:36 +0530

    module changes

--- src/app/pages/pickup-call/pickup-call.page.spec.ts
+++ src/app/pages/pickup-call/pickup-call.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { FolderPage } from './folder.page';
 
-describe('FolderPage', () => {
-  let component: FolderPage;
-  let fixture: ComponentFixture<FolderPage>;
+import { PickupCallPage } from './pickup-call.page';
+
+describe('PickupCallPage', () => {
+  let component: PickupCallPage;
+  let fixture: ComponentFixture<PickupCallPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ FolderPage ],
-      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
+      declarations: [ PickupCallPage ],
+      imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(FolderPage);
+    fixture = TestBed.createComponent(PickupCallPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
---
+++ src/app/pages/pickup-calls/pickup-calls.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PickupCallsPageRoutingModule } from './pickup-calls-routing.module';
+
+import { PickupCallsPage } from './pickup-calls.page';
+import { PickupCallCardComponent } from 'src/app/component/pickup-call-card/pickup-call-card.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PickupCallsPageRoutingModule
+  ],
+  declarations: [PickupCallsPage, PickupCallCardComponent]
+})
+export class PickupCallsPageModule {}
